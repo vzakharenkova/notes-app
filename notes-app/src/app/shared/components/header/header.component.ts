3fab2984e6161e8ad02581b8d0abfc27351167f7
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-11-23 12:46:39 +0300

    feat: create sidemenu

--- notes-app/src/app/shared/components/header/header.component.ts
+++ notes-app/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  @Input() sideNav!: MatDrawer;
 }
--- notes-app/src/app/shared/material.module.ts
+++ notes-app/src/app/shared/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
-  imports: [MatCardModule, MatIconModule],
-  exports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, MatSidenavModule, MatTooltipModule, MatButtonModule],
+  exports: [MatCardModule, MatIconModule, MatSidenavModule, MatTooltipModule, MatButtonModule],
 })
 export class MaterialModule {}
